a7bf2c8c343548ef40be1140867b427cecbe4d40
Nafid Faham <nafidfaham08@gmail.com>
2022-08-16 14:04:29 +0600

    classroom: Add user model for authentication -refactor code from dev branch - switched to .env varaiable input

---
+++ contollers/auth.controller.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+// Required for separate controller for user
+const mongoose = require("mongoose");
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  console.log(req.body);
+  try {
+    const newPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: newPassword,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    res.json({ status: "error", error: "Duplicate email" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (!user) {
+    return { status: "error", error: "Invalid login" };
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (isPasswordValid) {
+    const token = jwt.sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      "secret123"
+    );
+
+    return res.json({ status: "ok", user: token });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config({ path: "./config.env" });
 
+const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 
+const cors = require("cors");
+
+connectDB();
+app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+// Connecting Routes
+app.use("/api", require("./routes/auth.routes"));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Sever running on port ${PORT}`)
+);
